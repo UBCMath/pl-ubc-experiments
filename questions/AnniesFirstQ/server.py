b7d78fe7a98a29acbed43e71499298a8e4c1f231
Annie Geng <genga@ubc.ca>
2021-05-12 01:03:59 +0000

    AnniesFirstQ: edit server.py

--- questions/AnniesFirstQ/server.py
+++ questions/AnniesFirstQ/server.py
@@ -1,17 +1,20 @@
+import prairielearn as pl
+import sympy
 import random, copy
 
 def generate(data):
-
-    # Sample two random integers between 5 and 10 (inclusive)
+        # Sample two random integers between 5 and 10 (inclusive)
     a = random.randint(5, 10)
     b = random.randint(5, 10)
+    c = random.randint(5, 10)
+    d = random.randint(5, 10)
 
-    # Put these two integers into data['params']
     data['params']['a'] = a
     data['params']['b'] = b
+    data['params']['c'] = c
+    data['params']['d'] = d
 
-    # Compute the sum of these two integers
-    c = a + b
-
-    # Put the sum into data['correct_answers']
-    data['correct_answers']['c'] = c
+    x = sympy.symbols('x')
+    z = a*sympy.sin(x)+b*sympy.cos(x)
+    data['correct_answer']['symbolic_math'] = pl.to_json(z)
+    
