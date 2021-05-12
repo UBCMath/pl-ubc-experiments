0578cf7cd18966523b98dff8c9101364150ae074
Morgan Arnold <mrarnold@ubc.ca>
2021-05-12 21:47:47 +0000

    MorgansFirstQ: edit server.py

--- questions/MorgansFirstQ/server.py
+++ questions/MorgansFirstQ/server.py
@@ -4,10 +4,11 @@ import random
 
 def generate(data):
     x = sympy.symbols("x")
+    C = sympy.symbols("C")
     n = random.randint(1, 5)
     
     data["params"]["n"] = n
 
-    I = sympy.integrate(n/(x**3-1), x)
+    I = sympy.integrate(n/(x**3-1), x) + C
 
     data["correct_answers"]["integral"] = pl.to_json(I)
