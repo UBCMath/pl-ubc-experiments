96549089a4ce9afbc6e0042d469ae5333038a2f8
Cadenze <borisowenli@gmail.com>
2021-05-12 16:16:43 -0700

    BorisFirstQ: trying from github
    Trying to make a commit directly to GitHub

--- questions/BorisFirstQ/server.py
+++ questions/BorisFirstQ/server.py
@@ -4,10 +4,11 @@ import random
 
 def generate(data):
     x = sympy.symbols('x')
+    C = sympy.symbols('C')
     n = random.randint(3, 9)
 
     data['params']['n'] = n
 
-    I = sympy.integrate(x**2*sympy.atan(n*x), x)
+    I = sympy.integrate(x**2*sympy.atan(n*x), x) + C
 
     data['correct_answers']['I'] = pl.to_json(I)
