e67f772f578d6559886527988be5abe3adc96dd0
Morgan Arnold <morgan.r.arnold@outlook.com>
2021-05-12 23:46:27 -0700

    Fix "+-" in SuperParameterized
    An attempt at fixing a bug in SuperParameterized causing negative values to appear like +- instead of just - in the LaTeX.

--- questions/SuperParameterized/server.py
+++ questions/SuperParameterized/server.py
@@ -25,6 +25,12 @@ def generate(data):
     data["params"]["denom_b"] = random_parameters[4]*random_parameters[7] + random_parameters[5]*random_parameters[6]
     data["params"]["denom_c"] = random_parameters[5]*random_parameters[7]
 
+    # a bit of weirdness necessary to avoid "+-" instead of just "-". this may not be a good way to do this
+    data["params"]["num_sign1"] = "" if data["params"]["num_b"] < 0 else "+"
+    data["params"]["num_sign2"] = "" if data["params"]["num_c"] < 0 else "+"
+    data["params"]["denom_sign1"] = "" if data["params"]["denom_b"] < 0 else "+"
+    data["params"]["denom_sign2"] = "" if data["params"]["denom_c"] < 0 else "+"
+
     data["params"]["limit"] = limit
 
     data["correct_answers"]["limit"] = pl.to_json(rational.subs(x, limit))
