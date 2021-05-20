e601e3f76cc8b4fbd8717e388bfb6c2ef55a2f05
genga <genga@student.ubc.ca>
2021-05-20 16:06:38 -0700

    fixed some notations and comments

--- questions/AnniesFirstQ/server.py
+++ questions/AnniesFirstQ/server.py
@@ -3,7 +3,7 @@ import sympy
 import random, copy
 
 def generate(data):
-        # Sample two random integers between 5 and 10 (inclusive)
+    # create random integers between 5 and 10 (inclusive)
     a = random.randint(5, 10)
     b = random.randint(5, 10)
     c = random.randint(5, 10)
