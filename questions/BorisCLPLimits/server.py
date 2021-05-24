f1837941f0bcdd0bc24281ce205f003cc4470153
Cadenze <borisowenli@gmail.com>
2021-05-24 15:47:44 -0700

    fix: removed the use of sympy
    The easiest way is to simply hard-code it in, without any sympy/numpy

--- questions/BorisCLPLimits/server.py
+++ questions/BorisCLPLimits/server.py
@@ -1,15 +1,11 @@
-import sympy
 import random
 
 def generate(data):
-    x = sympy.symbols('x', real=True)
+
+    # Generate a random number that determines the y-intercept of the function
     n = random.randint(1, 9)
 
     data['params']['n'] = n
 
-    f = sympy.Piecewise((x**2-n, x < 0), (x**2+n, x > 0))
-
-    # answers currently hard-coded because I can't figure out how to "pl.to_json"-ify sympy integers
-    # if possible, using sympy.limit?
     data['correct_answers']['lim-L'] = -n
     data['correct_answers']['lim-R'] = n
