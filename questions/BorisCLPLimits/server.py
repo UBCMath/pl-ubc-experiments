dd595a83a22f9c36ffe403bbfc1cfabfe51b7453
Cadenze <borisowenli@gmail.com>
2021-05-24 15:21:21 -0700

    fix: added comment about possible update
    Added recommendation on what to change potentially (i.e. using sympy.limit instead of hard-coding in answer)

--- questions/BorisCLPLimits/server.py
+++ questions/BorisCLPLimits/server.py
@@ -10,5 +10,6 @@ def generate(data):
     f = sympy.Piecewise((x**2-n, x < 0), (x**2+n, x > 0))
 
     # answers currently hard-coded because I can't figure out how to "pl.to_json"-ify sympy integers
+    # if possible, using sympy.limit?
     data['correct_answers']['lim-L'] = -n
     data['correct_answers']['lim-R'] = n
