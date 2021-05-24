c4937b51f6ec7d3d2f47a36f099f1bb4507f28be
Cadenze <borisowenli@gmail.com>
2021-05-24 15:10:01 -0700

    feat(question): create new limits question
    New limits question, from CLP1 1.5 Q49. Constant in equation (y-intercept?) is parametrized.

---
+++ questions/BorisCLPLimits/server.py
@@ -0,0 +1,14 @@
+import sympy
+import random
+
+def generate(data):
+    x = sympy.symbols('x', real=True)
+    n = random.randint(1, 9)
+
+    data['params']['n'] = n
+
+    f = sympy.Piecewise((x**2-n, x < 0), (x**2+n, x > 0))
+
+    # answers currently hard-coded because I can't figure out how to "pl.to_json"-ify sympy integers
+    data['correct_answers']['lim-L'] = -n
+    data['correct_answers']['lim-R'] = n
