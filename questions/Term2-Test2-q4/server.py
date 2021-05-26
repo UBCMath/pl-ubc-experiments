c46c04724c5954f717a7ffb45a8a12bf651bf6f0
Annie Geng <anniegeng@Annies-Mac-mini.local>
2021-05-26 01:21:04 -0700

    Attempt to create semicircle on top of base

--- questions/Term2-Test2-q4/server.py
+++ questions/Term2-Test2-q4/server.py
@@ -5,7 +5,7 @@ import io
 import random
 import sympy
 import numpy as np
-
+    
 def file(data):
     if data['filename']=='figure.png':
         fig = plt.figure()
@@ -13,8 +13,8 @@ def file(data):
         R = 0.5
         scale = data['params']['scale']
         
+        
         #remove ticks on axes
-
         plt.tick_params(
             axis='both',          
             which='both',      
@@ -24,12 +24,23 @@ def file(data):
             right=False,
             left=False,
             labelleft=False)
-            
+        
+        #remove border on graph
+        fig.patch.set_visible(False)
+        ax.axis('off')
+        
         # add initial base
         x = [-R, -R, R,R]
         y = [0, scale*R, scale*R, 0]
         plt.plot(x,y)
         
+        theta = np.linspace(0, (np.pi/2), 100)
+        
+        x1 = R*np.cos(theta)
+        x2 = R*np.sin(theta) + scale*R
+
+        
+        ax.plot(x1, x2)
         
         plt.show()  
 
