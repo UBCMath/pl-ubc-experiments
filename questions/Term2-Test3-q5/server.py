d1914a3352ba232c0f62f0ae4b60e706d2c7566d
genga <genga@student.ubc.ca>
2021-05-30 15:24:29 -0700

    feat(question): Test 2.3 q5 using pl-overlay

---
+++ questions/Term2-Test3-q5/server.py
@@ -0,0 +1,10 @@
+import prairielearn as pl
+
+
+def generate (data):
+
+    data['correct_answers']['c'] = 1
+    data['correct_answers']['c2'] = 34680
+    data['correct_answers']['c3'] = 0
+    
+    
