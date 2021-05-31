9108017abc1798185f48cc2e9d5d4bf64203b49c
Annie Geng <anniegeng@Annies-Mac-mini.local>
2021-05-30 18:23:15 -0700

    fix: let blank-value be random

--- questions/Term2-Test3-q5/server.py
+++ questions/Term2-Test3-q5/server.py
@@ -1,7 +1,11 @@
+import random
 def generate (data):
+    b = random.randint(100000,900000)
+
+    data['params']['blank'] = b
 
     data['correct_answers']['c'] = 1
-    data['correct_answers']['c2'] = 34680
+    data['correct_answers']['c2'] = b
     data['correct_answers']['c3'] = 0
     
     
