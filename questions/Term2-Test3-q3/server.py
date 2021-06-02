92854e0cfafb7373f37b5cf744fea1ad3a6b27b3
Annie Geng <anniegeng@Annies-Mac-mini.local>
2021-06-02 13:40:29 -0700

    feat(question): Test 2.3 q3

---
+++ questions/Term2-Test3-q3/server.py
@@ -0,0 +1,25 @@
+import prairielearn as pl
+import sympy
+import random, copy
+
+def generate (data):
+    b = random.randint(2,4)
+    vary = random.randint(1,10)
+    arr = ["half", "a third", "a quarter"]
+    scale = arr [b-2] 
+
+    data['params']['scale'] = scale
+    data['params']['vary'] = vary
+    H = sympy.symbols("H")
+    y = sympy.symbols("y")
+    A = sympy.symbols("A")
+    c = sympy.symbols("c")
+    g = sympy.symbols("g")
+    z = 0*H
+    
+    ans = sympy.simplify(g*A*(vary+c*(H/b-y))*(H-y))
+    data['correct_answers']['symbolic_math'] = pl.to_json(ans)
+    data['correct_answers']['c3'] = pl.to_json(H/b)
+    data['correct_answers']['c4'] = pl.to_json(z)
+    
+    
