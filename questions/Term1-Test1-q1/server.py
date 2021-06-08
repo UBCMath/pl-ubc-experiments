eb69ce71e40f17d1c1f95e35b046ea789a92190c
genga <genga@student.ubc.ca>
2021-06-08 15:48:09 -0700

    fix: 1/(x-const) undefined error

--- questions/Term1-Test1-q1/server.py
+++ questions/Term1-Test1-q1/server.py
@@ -1,6 +1,7 @@
 import prairielearn as pl
 import sympy
 import random, copy
+from sympy.calculus.util import continuous_domain
 
 def generate (data):
     const = random.randint(1,10)
@@ -10,18 +11,17 @@ def grade (data):
     x = sympy.symbols ('x')
     const = data['params']['const']
     ans = pl.from_json(data['submitted_answers']['symbolic_math'])
-    limit_to_const = sympy.limit (ans, x, const)
-    f = sympy.lambdify (x, ans)
-    f_at_const = f(const)
     limit_to_const_inv = sympy.limit ((1/ans), x, const)
-
-    continuous = True if (limit_to_const == f_at_const) else False
-    limit_qualifies = True if (limit_to_const_inv == sympy.oo) else False
-
-        
-    if continuous and limit_qualifies:
-        data ['score'] = 1
-    else:
+    limit_qualifies = (limit_to_const_inv == sympy.oo)
+    if limit_qualifies:
+        limit_to_const = sympy.limit (ans, x, const)
+        f = sympy.lambdify (x, ans)
+        f_at_const = f(const)
+        if (limit_to_const == f_at_const):
+            data ['score'] = 1
+        else:
+            data ['score'] = 0
+    else: 
         data ['score'] = 0
-        
+
         
