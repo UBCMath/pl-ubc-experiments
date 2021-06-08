fa1d7f96a9de1c0d6dad5db8cda65299be02b265
genga <genga@student.ubc.ca>
2021-06-08 15:05:50 -0700

    feat(question): Test 1.1 q1

---
+++ questions/Term1-Test1-q1/server.py
@@ -0,0 +1,31 @@
+import prairielearn as pl
+from sympy import *
+import random, copy
+
+def generate (data):
+    const = random.randint(1,10)
+    data['params']['const'] = const
+
+def grade (data):
+    x = symbols ('x')
+    const = data['params']['const']
+    ans = pl.from_json(data['submitted_answers']['symbolic_math'])
+    limit_to_const = limit (ans, x, const)
+    f = lambdify (x, ans)
+    f_at_const = f(const)
+    limit_to_const_inv = limit ((1/ans), x, const)
+
+    continuous = True if (limit_to_const == f_at_const) else False
+    limit_qualifies = True if (limit_to_const_inv == oo) else False
+
+        
+    if continuous and limit_qualifies:
+        data ['score'] = 1
+    elif continous:
+        data ['score'] = 0.5
+    elif limit_qualifies:
+        data ['score'] = 0.5
+    else:
+        data ['score'] = 0
+        
+        
