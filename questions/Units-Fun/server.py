23a5f0253fa92ef54024823e9ffe87d69b16dd51
Cadenze <borisowenli@gmail.com>
2021-06-16 16:03:37 -0700

    feat(question): units
    Question about units. ability to find SI unit equivalency in terms of dimensions.

---
+++ questions/Units-Fun/server.py
@@ -0,0 +1,122 @@
+import math
+import re
+
+# From Wikipedia:
+#   The derived units in the SI are formed by powers, products, or quotients of the
+#   base units and are potentially unlimited in number.
+
+# To compare these units, we can represent these units as rational numbers. We know
+# all integers can be factorized down to prime numbers, and therefore every unit can
+# be represented as fractions, the composite of base units, as primes.
+
+def base_units():
+    # initializes the base units as first 7 prime numbers
+    base = {
+            "s": 2,
+            "m": 3,
+            "kg": 5,
+            "A": 7,
+            "K": 11,
+            "mol": 13,
+            "cd": 17
+        }
+    return base
+
+def derived_units():
+    # initializes the derived units as numberified powers of base unit primes
+    derived = {
+        "rad": 1,
+        "sr": 1,
+        "Hz": numberify_base("1/s"),
+        "N": numberify_base("kg*m/s^2"),
+        "Pa": numberify_base("kg/(m*s^2)"),
+        "J": numberify_base("kg*m^2/s^2"),
+        "W": numberify_base("kg*m^2/s^3"),
+        "C": numberify_base("A*s"),
+        "V": numberify_base("kg*m^2/(A*s^3)"),
+        "F": numberify_base("A^2*s^4/kg/m^2"),
+        "O": numberify_base("kg*m^2/s^3/A^2"), # decided that Ω = O
+        "Ω": numberify_base("kg*m^2/s^3/A^2"),
+        "S": numberify_base("A^2*s^3/kg/m^2"),
+        "Wb": numberify_base("kg*m^2/A/s^2"),
+        "T": numberify_base("kg/s^2/A"),
+        "H": numberify_base("kg*m^2/s^2/A^2"),
+        "lm": numberify_base("cd"),
+        "lx": numberify_base("cd/m^2"),
+        "Bq": numberify_base("1/s"),
+        "Gy": numberify_base("m^2/s^2"),
+        "Sv": numberify_base("m^2/s^2"),
+        "kat": numberify_base("mol/s")
+    }
+    return derived
+
+def numberify_base(unit):
+    # splits the unit strings
+    components = list(map((lambda a : base_units().get(a, a)), re.split("[*\/^()]", unit)))
+    operations = list(re.sub("[^*\/^()]", "", unit))
+    string = ""
+
+    # replaces units with number
+    for i in range(len(operations)):
+        string = string + str(components[i]) + operations[i]
+    string = string + str(components[-1])
+
+    # evaluate expression
+    return eval(string.replace("^", "**"))
+
+def numberify(unit):
+    # combines base and derived units
+    units = base_units().copy()
+    units.update(derived_units())
+
+    # splits the unit strings
+    components = list(map((lambda a : units.get(a, a)), re.split("[*\/^()]", unit)))
+    operations = list(re.sub("[^*\/^()]", "", unit))
+    string = ""
+
+    # replaces units with number
+    for i in range(len(operations)):
+        string = string + str(components[i]) + operations[i]
+    string = string + str(components[-1])
+
+    # evaluate expression
+    return eval(string.replace("^", "**"))
+
+def generate(data):
+    # initialize sample answer units
+    data['params']['t'] = "s"
+    data['params']['F'] = "N"
+    data['params']['j'] = "A/m^2"
+
+def grade(data):
+    # gets both sample and student answer
+    sample_t = data['params']['t']
+    sample_F = data['params']['F']
+    sample_j = data['params']['j']
+    ans_t = data['submitted_answers']['t']
+    ans_F = data['submitted_answers']['F']
+    ans_j = data['submitted_answers']['j']
+
+    # check whether they are identical or numerically same
+    if sample_t == ans_t or math.isclose(numberify(sample_t), numberify(ans_t)):
+        data['partial_scores']['t'] = {'score': 1, 'weight': 1}
+    else:
+        data['partial_scores']['t'] = {'score': 0, 'weight': 1}
+    
+    if sample_F == ans_F or math.isclose(numberify(sample_F), numberify(ans_F)):
+        data['partial_scores']['F'] = {'score': 1, 'weight': 1}
+    else:
+        data['partial_scores']['F'] = {'score': 0, 'weight': 1}
+    
+    if sample_j == ans_j or math.isclose(numberify(sample_j), numberify(ans_j)):
+        data['partial_scores']['j'] = {'score': 1, 'weight': 1}
+    else:
+        data['partial_scores']['j'] = {'score': 0, 'weight': 1}
+    
+    # calculate score
+    correct = 0
+    variables = data['partial_scores'].keys()
+    for name in variables:
+        correct += data['partial_scores'][name]['score']  
+    data['score'] = correct / len(variables)
+    
