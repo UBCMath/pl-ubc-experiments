75e4de925458d27872abe0455cdce4066631775e
Annie Geng <genga@ubc.ca>
2021-07-07 21:04:11 -0700

    feat: added randomization

--- questions/tutorialDerivatives_1/server.py
+++ questions/tutorialDerivatives_1/server.py
@@ -3,16 +3,18 @@ import sympy
 import random, copy
 
 def generate(data):
-    # create random integers between 5 and 10 (inclusive)
-    a = random.randint(5, 10)
-    b = random.randint(5, 10)
+    a = random.randint(2, 10)
+    b = random.randint(2, 10)
+    c = random.randint(1, 10)
+
 
     data['params']['a'] = a
     data['params']['b'] = b
+    data['params']['c'] = c
 
-    sympy.var('x')
+    sympy.var('a b c x')
 
-    data['correct_answers']['sin'] = pl.to_json(sympy.diff(sympy.sin(x**2), x, 2))
-    data['correct_answers']['cos'] = pl.to_json(sympy.diff(sympy.cos(sympy.sqrt(x)), x, 2))
-    data['correct_answers']['frac'] = pl.to_json(sympy.diff(1/(x+1)**5, x, 2))
+    data['correct_answers']['sin'] = pl.to_json(sympy.diff(sympy.sin(a*x**2), x, 2))
+    data['correct_answers']['cos'] = pl.to_json(sympy.diff(sympy.cos(sympy.sqrt(b*x)), x, 2))
+    data['correct_answers']['frac'] = pl.to_json(sympy.diff(c/(x+1)**5, x, 2))
     
