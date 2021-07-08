3700d36183698780a83a38a840db5102f1f26ec0
Annie Geng <genga@ubc.ca>
2021-07-07 21:32:06 -0700

    feat: selects one problem from bank and displays it

--- questions/tutorialDerivatives_1/server.py
+++ questions/tutorialDerivatives_1/server.py
@@ -6,15 +6,25 @@ def generate(data):
     a = random.randint(2, 10)
     b = random.randint(2, 10)
     c = random.randint(1, 10)
+    ind = ['display_a', 'display_b', 'display_c']
 
+    to_true = random.choices (ind, k =1) [0]
+    sympy.var('a b c x')
+    q_and_a = {
+        'display_a' : pl.to_json(sympy.diff(sympy.sin(a*x**2), x, 2)),
+        'display_b' : pl.to_json(sympy.diff(sympy.cos(sympy.sqrt(b*x)), x, 2)),
+        'display_c' : pl.to_json(sympy.diff(c/(x+1)**5, x, 2))
+    }
 
     data['params']['a'] = a
     data['params']['b'] = b
     data['params']['c'] = c
 
-    sympy.var('a b c x')
+    data['params']['display_a'] = False
+    data['params']['display_b'] = False
+    data['params']['display_c'] = False
+    #only displays the randomly selected answer 
+    data['params'][to_true] = True
 
-    data['correct_answers']['sin'] = pl.to_json(sympy.diff(sympy.sin(a*x**2), x, 2))
-    data['correct_answers']['cos'] = pl.to_json(sympy.diff(sympy.cos(sympy.sqrt(b*x)), x, 2))
-    data['correct_answers']['frac'] = pl.to_json(sympy.diff(c/(x+1)**5, x, 2))
+    data['correct_answers']['ans'] = q_and_a[to_true]
     
