70d245c15da16d28d8f8901eb5a2da32a836cab5
Annie Geng <genga@ubc.ca>
2021-07-19 14:46:41 -0700

    fix: simplified code for tutorialDerivatives_1

--- questions/tutorialDerivatives_1/server.py
+++ questions/tutorialDerivatives_1/server.py
@@ -6,25 +6,18 @@ def generate(data):
     a = random.randint(2, 10)
     b = random.randint(2, 10)
     c = random.randint(1, 9)
-    ind = ['display_a', 'display_b', 'display_c']
+    sympy.var('a b c x')
+    ind = [pl.to_json(sympy.sin(a*x**2)), pl.to_json(sympy.cos(b*x)), pl.to_json((c/(x+1)**5)) ]
+
 
     to_true = random.choices (ind, k =1) [0]
-    sympy.var('a b c x')
-    q_and_a = {
-        'display_a' : pl.to_json(sympy.diff(sympy.sin(a*x**2), x, 2)),
-        'display_b' : pl.to_json(sympy.diff(sympy.cos(sympy.sqrt(b*x)), x, 2)),
-        'display_c' : pl.to_json(sympy.diff(c/(x+1)**5, x, 2))
-    }
 
     data['params']['a'] = a
     data['params']['b'] = b
     data['params']['c'] = c
 
-    data['params']['display_a'] = False
-    data['params']['display_b'] = False
-    data['params']['display_c'] = False
-    #only displays the randomly selected answer 
-    data['params'][to_true] = True
+    #only displays the randomly selected question 
+    data['params']['display_this'] = sympy.latex(pl.from_json(to_true))
 
-    data['correct_answers']['ans'] = q_and_a[to_true]
+    data['correct_answers']['ans'] = pl.to_json(sympy.diff(pl.from_json(to_true),x,2))
     
