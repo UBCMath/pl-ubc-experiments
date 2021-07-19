0fc49948b468fdd9bdd3c2ccd74f97abf4028756
Cadenze <borisowenli@gmail.com>
2021-07-18 18:13:33 -0700

    refactor: tuple -> string for inputting answers
    maybe changeover to native `DimensionfulQuantity` soon?

--- questions/UnitMadness/server.py
+++ questions/UnitMadness/server.py
@@ -1,3 +1,3 @@
 def generate(data):
-    data['correct_answers']['capacitance'] = (1, "F")
-    data['correct_answers']['length'] = (1, "m")
+    data['correct_answers']['capacitance'] = "1 F"
+    data['correct_answers']['length'] = "1 m"
