1e219d29254c1a04502e4a4e498ecae83e8873fb
Annie Geng <genga@ubc.ca>
2021-07-27 14:43:45 -0700

    fix: redudant code with regards to pl.to_json

--- questions/tutorialDerivatives_1/server.py
+++ questions/tutorialDerivatives_1/server.py
@@ -7,17 +7,17 @@ def generate(data):
     b = random.randint(2, 10)
     c = random.randint(1, 9)
     sympy.var('a b c x')
-    ind = [pl.to_json(sympy.sin(a*x**2)), pl.to_json(sympy.cos(b*x)), pl.to_json((c/(x+1)**5)) ]
+    ind = [sympy.sin(a*x**2), sympy.cos(b*x), (c/(x+1)**5)]
 
 
-    to_true = random.choices (ind, k =1) [0]
+    choose_one = pl.to_json(random.choices (ind, k =1) [0])
 
     data['params']['a'] = a
     data['params']['b'] = b
     data['params']['c'] = c
 
     #only displays the randomly selected question 
-    data['params']['display_this'] = sympy.latex(pl.from_json(to_true))
+    data['params']['display_this'] = sympy.latex(pl.from_json(choose_one))
 
-    data['correct_answers']['ans'] = pl.to_json(sympy.diff(pl.from_json(to_true),x,2))
+    data['correct_answers']['ans'] = pl.to_json(sympy.diff(pl.from_json(choose_one),x,2))
     
