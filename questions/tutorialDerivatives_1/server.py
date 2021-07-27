ab291bc12c9042ea574769aa2a16414283233021
Annie Geng <genga@ubc.ca>
2021-07-27 15:48:25 -0700

    feat: added assert statement and solution.md file (manual solutions)

--- questions/tutorialDerivatives_1/server.py
+++ questions/tutorialDerivatives_1/server.py
@@ -6,18 +6,30 @@ def generate(data):
     a = random.randint(2, 10)
     b = random.randint(2, 10)
     c = random.randint(1, 9)
-    sympy.var('a b c x')
-    ind = [sympy.sin(a*x**2), sympy.cos(b*x), (c/(x+1)**5)]
+    sympy.var('a b c')
+    x = sympy.symbols('x',real=True)
+    
+    q_arr = [sympy.sin(a*x**2), sympy.cos(b*x), (c/(x+1)**5)]
+    manual_ans_arr = [-sympy.sin(a*x**2)*4*a**2*x**2+sympy.cos(a*x**2)*2*a,-b**2*sympy.cos(b*x), (30*c)/(x+1)**7]
+    
+    #randomly selects 0, 1, or 2 representing the selected index
+    index_to_choose = random.randint(0,2)
 
 
-    choose_one = pl.to_json(random.choices (ind, k =1) [0])
-
+    selected_q = q_arr[index_to_choose]
+    selected_ans = manual_ans_arr [index_to_choose]
+    
     data['params']['a'] = a
     data['params']['b'] = b
     data['params']['c'] = c
 
     #only displays the randomly selected question 
-    data['params']['display_this'] = sympy.latex(pl.from_json(choose_one))
+    data['params']['display_this'] = sympy.latex(selected_q)
+    
+    sympy_compute_ans = sympy.diff(selected_q,x,2)
+    
+    #checks whether sympy and manual computed answers match or not, if not error is raised to display the selected question
+    assert sympy.simplify (sympy.Eq (selected_ans,sympy_compute_ans)),selected_q
 
-    data['correct_answers']['ans'] = pl.to_json(sympy.diff(pl.from_json(choose_one),x,2))
+    data['correct_answers']['ans'] = pl.to_json(sympy_compute_ans)
     
---
+++ questions/tutorialDerivatives_1/solutions.md
@@ -0,0 +1,21 @@
+# Tutorial Derivatives 1 
+
+Find the second derivative.
+
+## sympy.sin(a*x**2)
+
+$$f'(x) = 2*a*x*cos(a*x^{2}) $$
+
+$$f''(x) = 2*a*cos(a*x^{2})-4*a^{2}*x^{2}*sin(a*x^{2})$$
+
+## sympy.cos(b*x)
+
+$$f'(x) = -sin(b*x)*b$$
+
+$$f''(x) = -b^{2}*cos(b*x)$$
+
+## (c/(x+1)**5)
+
+$$f'(x) = \frac{-5*c}{(x+1)^{6}} $$
+
+$$f''(x) = \frac{30*c}{(x+1)^{7}} $$
