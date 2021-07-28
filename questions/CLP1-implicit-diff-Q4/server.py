3eebfc12379b5e13229d7f58e1dd891178f7ca85
Annie Geng <genga@ubc.ca>
2021-07-28 13:01:03 -0700

    feat: added solutions.md file

--- questions/CLP1-implicit-diff-Q4/server.py
+++ questions/CLP1-implicit-diff-Q4/server.py
@@ -4,15 +4,16 @@ import random, copy
 
 def generate(data):
 
-    a = random.randint(1, 9)
-    b = random.randint(1, 9)
-    c = random.randint(1, 9)
+    a = random.randint(2, 9)
+    b = random.randint(2, 9)
+    c = random.randint(2, 9)
     d = random.randint(1, 30)
 
     data['params']['a'] = a
     data['params']['b'] = b
     data['params']['c'] = c
     data['params']['d'] = d
+    
     s.var('a b c d')
     x = s.symbols('x',real=True)
     y = s.symbols('y',real=True)
@@ -21,6 +22,7 @@ def generate(data):
     manual_ans = -(b*s.exp(b*x)+a*y)/(a*x+c*s.exp(c*y))
     sympy_ans = s.idiff(q_lhs,y,x)
     
+    #checks if manual and sympy computed answers are the same
     assert (s.simplify(s.Eq (sympy_ans, manual_ans)))
 
     data['correct_answers']['ans'] = pl.to_json(manual_ans)
---
+++ questions/CLP1-implicit-diff-Q4/solutions.md
@@ -0,0 +1,11 @@
+# CLP1 2.12 Q4
+
+Find $\displaystyle \frac{dy}{dx}$ (using implicit differntiation)
+
+##  $axy + e^{b x} + e^{ c y} = d$ 
+
+$$ay +ax\frac{dy}{dx} +be^{bx} +ce^{cy}\frac{dy}{dx} =0$$
+
+$$(ax+ce^{cy}\frac{dy}{dx})\frac{dy}{dx}=-(be^{bx}+ay)$$
+
+$$\frac{dy}{dx}= \frac{-(be^{bx}+ay)} {ax+ce^{cy}}$$
