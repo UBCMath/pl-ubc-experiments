c85fef8837972fe2ce75231dda0b924e33c3ef7e
Annie Geng <genga@ubc.ca>
2021-07-28 12:50:17 -0700

    feat(question): initial commit

---
+++ questions/CLP1-implicit-diff-Q4/server.py
@@ -0,0 +1,26 @@
+import prairielearn as pl
+import sympy as s
+import random, copy
+
+def generate(data):
+
+    a = random.randint(1, 9)
+    b = random.randint(1, 9)
+    c = random.randint(1, 9)
+    d = random.randint(1, 30)
+
+    data['params']['a'] = a
+    data['params']['b'] = b
+    data['params']['c'] = c
+    data['params']['d'] = d
+    s.var('a b c d')
+    x = s.symbols('x',real=True)
+    y = s.symbols('y',real=True)
+    
+    q_lhs = a*x*y + s.exp(b*x) + s.exp(c*y)
+    manual_ans = -(b*s.exp(b*x)+a*y)/(a*x+c*s.exp(c*y))
+    sympy_ans = s.idiff(q_lhs,y,x)
+    
+    assert (s.simplify(s.Eq (sympy_ans, manual_ans)))
+
+    data['correct_answers']['ans'] = pl.to_json(manual_ans)
