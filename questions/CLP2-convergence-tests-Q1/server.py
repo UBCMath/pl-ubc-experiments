fa2447477c7c1c0d7118f80b91de3ee7e40b1b0b
Annie Geng <genga@ubc.ca>
2021-08-02 23:05:27 -0700

    fix: fixing sympy imports

--- questions/CLP2-convergence-tests-Q1/server.py
+++ questions/CLP2-convergence-tests-Q1/server.py
@@ -1,8 +1,8 @@
-import sympy
+from sympy import var, latex, Sum, oo, pi, sin
 
 def generate(data):
-    sympy.var('n')
-    data['params']['a'] = sympy.latex(sympy.Sum(1/n, (n,1,sympy.oo)))
-    data['params']['b'] = sympy.latex(sympy.Sum(n**2/(n+1), (n,1,sympy.oo)))
-    data['params']['c'] = sympy.latex(sympy.Sum(sympy.sin(n), (n,1,sympy.oo)))
-    data['params']['d'] = sympy.latex(sympy.Sum(sympy.sin(sympy.pi*n), (n,1,sympy.oo)))
+    var('n')
+    data['params']['a'] = latex(Sum(1/n, (n,1,oo)))
+    data['params']['b'] = latex(Sum(n**2/(n+1), (n,1,oo)))
+    data['params']['c'] = latex(Sum(sin(n), (n,1,oo)))
+    data['params']['d'] = latex(Sum(sin(pi*n), (n,1,oo)))
