73ed900249007f7d51be8b7ad699a3bb14e5cb0a
Annie Geng <genga@ubc.ca>
2021-08-10 17:21:53 -0700

    fix: fixed imports and added solution explanations

--- questions/Term1-Test1-q1/server.py
+++ questions/Term1-Test1-q1/server.py
@@ -1,7 +1,6 @@
 import prairielearn as pl
 from sympy import symbols, limit, oo
-import random, copy
-from sympy.calculus.util import continuous_domain
+import random
 
 def generate (data):
     const = random.randint(1,10)
@@ -10,12 +9,15 @@ def generate (data):
 def grade (data):
     x = symbols ('x')
     const = data['params']['const']
+    # stores valid student answer into ans
     ans = pl.from_json(data['submitted_answers']['symbolic_math'])
+    # checks whether the second condition is satisfied
     limit_qualifies = ((limit ((1/ans), x, const,'-') == oo) and (limit ((1/ans), x, const,'+') == oo))
     if limit_qualifies:
         limit_to_const = limit (ans, x, const)
         f_at_const = ans.subs (x, const)
         
+        # checks whether or not function is continous at x = const
         if (limit_to_const == f_at_const):
             data ['score'] = 1
         else:
