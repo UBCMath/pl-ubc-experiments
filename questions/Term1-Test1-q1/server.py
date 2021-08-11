97460a026a536ddc3740361345214a4ed2f2f330
Annie Geng <genga@ubc.ca>
2021-08-10 17:19:00 -0700

    fix: Term1-Test1-q1 functionality

--- questions/Term1-Test1-q1/server.py
+++ questions/Term1-Test1-q1/server.py
@@ -1,22 +1,21 @@
 import prairielearn as pl
-from sympy import Symbol, limit, oo, Eq, lambdify
+from sympy import symbols, limit, oo
 import random, copy
+from sympy.calculus.util import continuous_domain
 
 def generate (data):
     const = random.randint(1,10)
     data['params']['const'] = const
-    
 
 def grade (data):
-    x = Symbol('x', real=True)
+    x = symbols ('x')
     const = data['params']['const']
     ans = pl.from_json(data['submitted_answers']['symbolic_math'])
     limit_qualifies = ((limit ((1/ans), x, const,'-') == oo) and (limit ((1/ans), x, const,'+') == oo))
-    
     if limit_qualifies:
         limit_to_const = limit (ans, x, const)
         f_at_const = ans.subs (x, const)
-
+        
         if (limit_to_const == f_at_const):
             data ['score'] = 1
         else:
