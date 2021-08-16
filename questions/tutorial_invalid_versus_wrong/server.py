8c3161b16df80e77e6b8bb14143420caa3c06d7d
Annie Geng <genga@ubc.ca>
2021-08-16 15:01:08 -0700

    fix: bug with round(), change to math.ceil instead

--- questions/tutorial_invalid_versus_wrong/server.py
+++ questions/tutorial_invalid_versus_wrong/server.py
@@ -1,6 +1,7 @@
 import prairielearn as pl
 from sympy import symbols, Rational
 import random
+import math
 
 def generate(data):
     a = random.choices ([9,11,13,15])[0]
@@ -12,7 +13,7 @@ def generate(data):
     x = symbols('x')
     
     data['correct_answers']['num'] = a/b
-    data['correct_answers']['int_value'] = round(a/b)
+    data['correct_answers']['int_value'] = math.ceil(a/b)
     data['correct_answers']['sym'] = pl.to_json(Rational(a/b))
 
     
