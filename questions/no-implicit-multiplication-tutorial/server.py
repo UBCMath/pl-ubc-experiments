c47f08b6e1b58f9b2037b727e46330a7d7f8a2b7
Annie Geng <genga@ubc.ca>
2021-08-31 01:06:47 -0700

    fix: clean up code

--- questions/no-implicit-multiplication-tutorial/server.py
+++ questions/no-implicit-multiplication-tutorial/server.py
@@ -6,11 +6,10 @@ def generate(data):
     # create a random integer between 5 and 10 (inclusive)
     a = random.randint(5, 10)
 
-    var('x t')
+    var('x')
     
     data['params']['a'] = a
     data['params']['formula'] = latex(a*cos(a*x))
-    data['params']['wrong_formula'] = latex(a*cos(a*t))
     data['correct_answers']['sym_1'] = pl.to_json(a*cos(a*x))
 
     
