1a0280f02d466cb69102f4f9c19754d4ba4c749e
Colin B. Macdonald <cbm@m.fsf.org>
2021-09-02 10:14:42 -0700

    Use SymPy for rounding: gives round to even

--- questions/tutorial_invalid_versus_wrong/server.py
+++ questions/tutorial_invalid_versus_wrong/server.py
@@ -4,17 +4,19 @@ import random
 import math
 
 def generate(data):
-    a = random.choices ([9,11,13,15])[0]
+    a = random.choice([9, 11, 13, 15])
     b = 2
-    
+
     data['params']['a'] = a
     data['params']['b'] = b
-    
+
     x = symbols('x')
-    
+
+    q = Rational(a, b)
+    # .round has round-to-even behaviour, then convert to ‘int‘
+    n = int(Rational(a, b).round())
+
     data['correct_answers']['num'] = a/b
     data['correct_answers']['num_f'] = a/b
-    data['correct_answers']['int_value'] = math.ceil(a/b)
-    data['correct_answers']['sym'] = pl.to_json(Rational(a/b))
-
-    
+    data['correct_answers']['int_value'] = n
+    data['correct_answers']['sym'] = pl.to_json(q)
